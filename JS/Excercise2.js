b514b459de4fd657a2747de397c19e9dc4efab60
rushipraneeth <s.rushipraneeth@gmail.com>
2026-06-01 09:51:16 +0530

    Add files via upload

---
+++ JS/Excercise2.js
@@ -0,0 +1,19 @@
+// Event details
+const eventName = "Tech Conference 2026";
+const eventDate = "15-06-2026";
+
+// Available seats
+let availableSeats = 100;
+
+// Registration
+availableSeats--;
+
+// Display event information
+console.log(`Event Name: ${eventName}`);
+console.log(`Event Date: ${eventDate}`);
+console.log(`Available Seats: ${availableSeats}`);
+
+// New seats added
+availableSeats++;
+
+console.log(`Updated Available Seats: ${availableSeats}`);
